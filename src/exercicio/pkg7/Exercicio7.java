9bbabe2bd7539b4d4e9316b6e8caaffa43d49427
filip <filip@Filipe-M>
2018-02-15 15:03:07 -0200

    Exercicio 07 1.0.0

---
+++ src/exercicio/pkg7/Exercicio7.java
@@ -0,0 +1,36 @@
+package exercicio.pkg7;
+import java.util.Random;
+public class Exercicio7 {    
+    public static void main(String[] args) {
+        
+        int a[] = new int [30];
+        float mediaN = 0;
+        float mediaP = 0;
+        int contP = 0;
+        int contN = 0;
+        
+        Random random = new Random();
+        
+        for (int i=0; i<a.length; i++){
+        
+            a[i] = random.nextInt(40)-20;
+            System.out.println("v:"+i+"="+a[i]);
+            
+            if(a[i]<0){
+            mediaN+=a[i];
+            contN++;
+            }else{
+                
+            mediaP+=a[i];
+            contP++;
+                 }
+            
+        }
+        mediaN = mediaN/contN;
+        mediaP = mediaP/contP;
+        System.out.println("");
+        System.out.println(mediaP);
+        System.out.println(mediaN);
+    }
+    
+}
